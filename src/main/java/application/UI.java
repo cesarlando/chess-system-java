f5bab26062530d488c21e40a31ae747cb9adc2b9
cesar.lando <sgtlando@gmail.com>
2023-11-19 19:46:19 -0300

    Handling exceptions and clearig screen

--- src/main/java/application/UI.java
+++ src/main/java/application/UI.java
@@ -28,6 +28,11 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    public static void clearScreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
             String s = sc.nextLine();
@@ -50,13 +55,6 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece) {
-        if (piece == null) {
-            System.out.print("-");
-        }
-        else {
-            System.out.print(piece);
-        }
-        System.out.print(" ");
         if (piece == null) {
             System.out.print("-");
         }
