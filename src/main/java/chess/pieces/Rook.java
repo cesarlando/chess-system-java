c9afc88076339879e7a9cfcc8355fa96327b5945
cesar.lando <sgtlando@gmail.com>
2023-11-18 22:12:31 -0300

    Little improvement in board printing

--- src/main/java/chess/pieces/Rook.java
+++ src/main/java/chess/pieces/Rook.java
@@ -1,16 +1,16 @@
-package chesspieces;
+package chess.pieces;
 
 import boardgame.Board;
 import chess.ChessPiece;
 import chess.Color;
 
-public class King extends ChessPiece {
-    public King(Board board, Color color) {
+public class Rook extends ChessPiece {
+    public Rook(Board board, Color color) {
         super(board, color);
     }
 
     @Override
     public String toString() {
-        return "K";
+        return "R";
     }
 }
